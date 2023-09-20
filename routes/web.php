54ecba36c509b4498e9284fd6997c6db3b25d942
Presly21 <preslyebonke21@gmail.com>
2023-09-20 09:38:12 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TodoController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('todos/index',[App\Http\Controllers\TodoController::class,'index'])->name('todos.index');
