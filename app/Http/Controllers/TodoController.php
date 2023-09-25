87898dc5a206a1cf072ed761cbfea140069d4fe9
Presly21 <preslyebonke21@gmail.com>
2023-09-25 09:36:38 +0100

    deuxieme partie laravel

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\TodoRequest;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 class TodoController extends Controller
 {
     public function index(){
         return view('todos.index');
     }
+    public function create(){
+        return view('todos.create');
+    }
+    public function store(TodoRequest $request){
+        Todo::create([
+           'title'=>$request->title,
+           'description'=>$request->description,
+           'is_completed'=>0
+        ]);
+        // $request->session()->flash('alert-success' ,'Todo created successfully');
+        // return to_route('todos.index')->with('success', "Todo created successfully"); 
+        return redirect()->route('todos.index')->with('success', "Todo created successfully"); 
+    }
+
+
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,17 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Todos App</div>
 
-                <div class="card-body">
-                    Todos index page
+                    <div class="card-body">
+
+                        @if (session('success'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Completed</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
